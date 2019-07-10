09fc6bc757d4c819acbfb99e337d429e59d07911
Jon Mockbee <mockbee1000@gmail.com>
2019-07-10 14:21:27 -0400

    initial commit

---
+++ src/MegabytesConverter.java
@@ -0,0 +1,38 @@
+
+
+public class MegabytesConverter {
+
+    public static void (int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println( "invalid value");
+        } else {
+            int megaBytes = (kiloBytes / 1024);
+            
+
+    }
+
+
+}
+
+
+//    public static long  toMilesPerHour(double kilometersPerHour){
+//
+//        if (kilometersPerHour <0){
+//            return -1;
+//        }
+//
+//        return Math.round(kilometersPerHour/1.609);
+//
+//    }
+//    public static void printConversion(double kilometersPerHour) {
+//
+//        if (kilometersPerHour <0) {
+//            System.out.println( "invalid value");
+//        } else {
+//            long milesPerHour = toMilesPerHour(kilometersPerHour);
+//            System.out.println(kilometersPerHour+" km/h = " + milesPerHour +
+//                    " mi/h");
+//        }
+//
+//    }
+//}
